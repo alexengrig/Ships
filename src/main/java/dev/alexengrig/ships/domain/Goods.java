b6106613cb592308e3b22509dc97d09a9d165027
Grig Alex <alexengrigdev@gmail.com>
2020-04-29 00:46:18 +0300

    Update Goods

--- src/main/java/dev/alexengrig/ships/domain/Goods.java
+++ src/main/java/dev/alexengrig/ships/domain/Goods.java
@@ -1,13 +1,33 @@
 package dev.alexengrig.ships.domain;
 
+import java.util.Objects;
+
 public class Goods<T> {
     private final T value;
 
     public Goods(T value) {
-        this.value = value;
+        this.value = Objects.requireNonNull(value, "Value is null");
     }
 
     public T getValue() {
         return value;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Goods<?> goods = (Goods<?>) o;
+        return value.equals(goods.value);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(value);
+    }
+
+    @Override
+    public String toString() {
+        return value.toString();
     }
 }
