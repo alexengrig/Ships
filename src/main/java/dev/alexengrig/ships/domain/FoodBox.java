77b62dfd32241ddd79ede9fc120ae1f3cb36c5c6
Grig Alex <alexengrigdev@gmail.com>
2020-04-29 19:32:10 +0300

    Remove FoodBox

--- src/main/java/dev/alexengrig/ships/domain/FoodBox.java
+++
@@ -1,7 +0,0 @@
-package dev.alexengrig.ships.domain;
-
-public class FoodBox extends Goods<Food> {
-    public FoodBox(Food value) {
-        super(value);
-    }
-}
--- src/main/java/dev/alexengrig/ships/factory/FoodFactory.java
+++ src/main/java/dev/alexengrig/ships/factory/FoodFactory.java
@@ -1,12 +1,11 @@
 package dev.alexengrig.ships.factory;
 
 import dev.alexengrig.ships.domain.Food;
-import dev.alexengrig.ships.domain.FoodBox;
 import dev.alexengrig.ships.domain.Goods;
 
 public class FoodFactory implements GoodsFactory<Food> {
     @Override
     public Goods<Food> createGoods(Food food) {
-        return new FoodBox(food);
+        return new Goods<>(food);
     }
 }
