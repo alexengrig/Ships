29d0f5371cc9dc149f28c52c06b59b16b4fc59d1
Grig Alex <alexengrigdev@gmail.com>
2020-04-29 21:41:40 +0300

    Update Runner

--- src/main/java/dev/alexengrig/ships/Runner.java
+++ src/main/java/dev/alexengrig/ships/Runner.java
@@ -4,22 +4,28 @@ import dev.alexengrig.ships.domain.Food;
 import dev.alexengrig.ships.domain.Ship;
 import dev.alexengrig.ships.factory.FoodFactory;
 import dev.alexengrig.ships.factory.FoodShipFactory;
-import dev.alexengrig.ships.factory.GoodsFactory;
-import dev.alexengrig.ships.factory.ShipFactory;
-import dev.alexengrig.ships.generator.*;
+import dev.alexengrig.ships.generator.RandomFoodGenerator;
+import dev.alexengrig.ships.generator.RandomFoodShipGenerator;
+import dev.alexengrig.ships.generator.ShipGenerator;
+import dev.alexengrig.ships.loader.SlowShipLoader;
 
 public class Runner {
     public static void main(String[] args) {
-        ShipGenerator<Food> shipGenerator = createFullShipGenerator();
-        Ship<Food> ship = shipGenerator.generate();
+        ShipGenerator<Food> producer = createGenerator();
+        Ship<Food> ship = producer.generate();
+        System.out.println("Before load:");
+        System.out.println(ship);
+        SlowShipLoader<Food> consumer = createLoader();
+        consumer.load(ship);
+        System.out.println("After load:");
         System.out.println(ship);
     }
 
-    private static ShipGenerator<Food> createFullShipGenerator() {
-        ShipFactory<Food> shipFactory = new FoodShipFactory();
-        ShipGenerator<Food> shipGenerator = new RandomFoodShipGenerator(shipFactory, 1, 10);
-        GoodsFactory<Food> goodsFactory = new FoodFactory();
-        GoodsGenerator<Food> goodsGenerator = new RandomFoodGenerator(goodsFactory);
-        return new FullShipGenerator<>(shipGenerator, goodsGenerator);
+    public static SlowShipLoader<Food> createLoader() {
+        return new SlowShipLoader<>(new RandomFoodGenerator(new FoodFactory()));
+    }
+
+    private static ShipGenerator<Food> createGenerator() {
+        return new RandomFoodShipGenerator(new FoodShipFactory(), 1, 5);
     }
 }
