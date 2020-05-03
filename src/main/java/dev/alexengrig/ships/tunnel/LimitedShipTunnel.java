b334b195b1bd60a296232fcd8784b81a04cba565
Grig Alex <alexengrigdev@gmail.com>
2020-05-03 23:37:37 +0300

    Update LimitedShipTunnel

--- src/main/java/dev/alexengrig/ships/tunnel/LimitedShipTunnel.java
+++ src/main/java/dev/alexengrig/ships/tunnel/LimitedShipTunnel.java
@@ -1,25 +1,9 @@
 package dev.alexengrig.ships.tunnel;
 
-import dev.alexengrig.ships.domain.Ship;
-
-import java.util.Queue;
-
 public abstract class LimitedShipTunnel<T> implements ShipTunnel<T> {
-    protected final Queue<Ship<T>> queue;
+    protected final int limit;
 
     protected LimitedShipTunnel(int limit) {
-        queue = createQueue(limit);
-    }
-
-    protected abstract Queue<Ship<T>> createQueue(int limit);
-
-    @Override
-    public void push(Ship<T> ship) {
-        queue.offer(ship);
-    }
-
-    @Override
-    public Ship<T> pull() {
-        return queue.poll();
+        this.limit = limit;
     }
 }
